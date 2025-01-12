679ea6c590e8cd752986e6bf3c957174a1883b8c
biyash <sthaalish39@gmail.com>
2025-01-12 15:31:42 +0545

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,24 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header("Authorization")?.replace("Bearer ", "");
   
   if (!token) {
-    console.log('No token provided');
-    return res.status(401).json({ msg: 'No token provided' });
+    console.log("No token provided");
+    return res.status(401).json({ msg: "No token provided" });
   }
+
+  console.log("Token received:", token); // Log the received token
+  console.log("JWT Secret:", process.env.JWT_SECRET); // Log the JWT secret
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded Token:", decoded); // Log decoded token
     req.user = decoded;
-    console.log('Decoded User:', decoded); // Verify the decoded user
     next();
   } catch (error) {
-    console.error('Invalid token error:', error);
-    res.status(401).json({ msg: 'Invalid token' });
+    console.error("Invalid token error:", error.message);
+    res.status(401).json({ msg: "Invalid token", error: error.message });
   }
 };
 
